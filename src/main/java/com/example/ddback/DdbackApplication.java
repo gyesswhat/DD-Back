635afd11717b13aef431c753e9b2ce4cb4752e79
gyesswhat <h2weirdo@icloud.com>
2024-07-17 19:56:39 +0900

    chore: 초기 파일 세팅

---
+++ src/main/java/com/example/ddback/DdbackApplication.java
@@ -0,0 +1,13 @@
+package com.example.ddback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ddback/DdbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ddback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
