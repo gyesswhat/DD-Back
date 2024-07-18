c993a2e4be47231d64715083af09bdca936deaa5
gyesswhat <h2weirdo@icloud.com>
2024-07-19 00:45:49 +0900

    chore: 배포 관련 작업 추가

--- src/main/java/com/example/ddback/config/WebConfig.java
+++ src/main/java/com/example/ddback/config/WebConfig.java
@@ -1,7 +1,14 @@
 package com.example.ddback.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 경로에 대해 CORS 허용 설정
+                .allowedOrigins("https://2024-ewha-unis-festival-booth.vercel.app/", "http://localhost:3030/"); // 허용할 origin 목록
+    }
 }
