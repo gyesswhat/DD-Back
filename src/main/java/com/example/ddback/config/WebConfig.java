0ceadc62825cca35455422197f2f6b456a8dc001
gyesswhat <h2weirdo@icloud.com>
2024-07-19 00:50:08 +0900

    chore: 배포 관련 작업 추가

--- src/main/java/com/example/ddback/config/WebConfig.java
+++ src/main/java/com/example/ddback/config/WebConfig.java
@@ -9,6 +9,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해 CORS 허용 설정
-                .allowedOrigins("https://2024-ewha-unis-festival-booth.vercel.app/", "http://localhost:3030/"); // 허용할 origin 목록
+                .allowedOrigins("https://2024-ewha-unis-festival-booth.vercel.app/", "http://localhost:3000/"); // 허용할 origin 목록
     }
 }
