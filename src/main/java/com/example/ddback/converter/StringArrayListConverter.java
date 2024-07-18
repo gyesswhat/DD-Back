8c3c0d71813ae54f020ca47f7ba36615f8991dbd
gyesswhat <h2weirdo@icloud.com>
2024-07-18 21:58:18 +0900

    chore: 배포 관련 작업 추가

--- src/main/java/com/example/ddback/converter/StringArrayListConverter.java
+++ src/main/java/com/example/ddback/converter/StringArrayListConverter.java
@@ -1,4 +1,4 @@
-package com.example.ddback;
+package com.example.ddback.converter;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
